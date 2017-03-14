61cb1e39567520a172444923d082b2ebb1e3d6ab
Hans Kurniadi <hans.d.k@live.com>
2017-03-14 00:16:44 +0000

    task 4, using attr_writer as a setter

--- person.rb
+++ person.rb
@@ -1,5 +1,6 @@
 class Person
   attr_reader :name
+  attr_writer :name
 
   def initialize(name)
     @name = name
@@ -9,9 +10,9 @@ class Person
   #   @name
   # end
 
-  def set_name(new_name)
-    @name = new_name
-  end
+  # def set_name(new_name)
+  #   @name = new_name
+  # end
 
 end
 
@@ -19,4 +20,5 @@ p = Person.new('L. Ron')
 
 # puts p.get_name
 # puts p.name
-# puts p.set_name("new naaame") 
+# puts p.set_name("new naaame")
+puts p.name = "neeew naaame"
