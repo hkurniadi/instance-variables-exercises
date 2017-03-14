4d90fd80c842a9eb76244a16b98ccbd36993f46b
Hans Kurniadi <hans.d.k@live.com>
2017-03-14 00:10:55 +0000

    task 2, using attr_reader for getter

--- person.rb
+++ person.rb
@@ -1,14 +1,17 @@
 class Person
+  attr_reader :name
 
   def initialize(name)
     @name = name
   end
 
-  def get_name
-    @name
-  end
+  # def get_name
+  #   @name
+  # end
 
 end
 
 p = Person.new('L. Ron')
-puts p.get_name
+
+# puts p.get_name
+puts p.name
